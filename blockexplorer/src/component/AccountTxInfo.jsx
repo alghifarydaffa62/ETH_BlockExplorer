e9104956f749baf8479cb93584bd6b3b6ba588b8
M Daffa A <daffa.alghifary62@gmail.com>
2025-07-31 10:00:19 +0700

    Style: Styling table transactions

--- blockexplorer/src/component/AccountTxInfo.jsx
+++ blockexplorer/src/component/AccountTxInfo.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import alchemy from "../lib/alchemy"
 import ShortHash from "../lib/shortenHash"
+import { Link } from "react-router-dom"
 
 export default function AccountTxInfo({address}) {
     const [txInfo, setTxInfo] = useState([])
@@ -11,7 +12,7 @@ export default function AccountTxInfo({address}) {
                 fromBlock: '0x0',
                 toBlock: 'latest',
                 fromAddress: address,
-                category: ['external', 'internal', 'erc20'],
+                category: ['external', 'internal', 'erc20', 'erc721', 'erc1155', 'specialnft'],
                 maxCount: 20,
                 order: 'asc'
             })
@@ -23,25 +24,30 @@ export default function AccountTxInfo({address}) {
     }, [address])
 
     return(
-        <div className="flex justify-center mt-8">
-            <table className="bg-[#0c1f3e] border-collapse border border-gray-400">
-                <th className="p-4">Transaction Hash</th>
-                <th>Block Number</th>
-                <th>Sender</th>
-                <th>Recipient</th>
-                <th>Value</th>
-                <th>Category</th>
-
-                {txInfo.map((tx, index) => (
-                    <tr key={index}>
-                        <td>{tx.hash.slice(0, 20)}...</td>
-                        <td>{tx.blockNum.toString()}</td>
-                        <td>{tx.to.slice(0, 20)}...</td>
-                        <td>{tx.from.slice(0, 20)}...</td>
-                        <td>{tx.value} {tx.asset}</td>
-                        <td>{tx.category}</td>
+        <div className="flex justify-center my-8">
+            <table className="w-9/10 bg-[#0c1f3e] text-white border border-gray-700 rounded-lg overflow-hidden shadow-md">
+                <thead>
+                    <tr className="bg-[#122c52] w-4/5">
+                        <th className="p-4 border-b border-gray-600">Transaction Hash</th>
+                        <th className="p-4 border-b border-gray-600">Block Number</th>
+                        <th className="p-4 border-b border-gray-600">From</th>
+                        <th className="p-4 border-b border-gray-600">To</th>
+                        <th className="p-4 border-b border-gray-600">Value</th>
+                        <th className="p-4 border-b border-gray-600">Category</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {txInfo.map((tx, index) => (
+                        <tr key={index} className="hover:bg-[#1a3766] transition-colors">
+                            <td className="py-4 px-6 border-b border-gray-600 hover:text-blue-400"><Link to={`/transaction/${tx.hash}`}>{tx.hash.slice(0, 20)}...</Link></td>
+                            <td className="py-4 px-6  border-b border-gray-600">{parseInt(tx.blockNum, 16)}</td>
+                            <td className="py-4 px-6  border-b border-gray-600">{tx.from?.slice(0, 20)}...</td>
+                            <td className="py-4 px-6  border-b border-gray-600 hover:text-blue-400"><Link to={`/Account/${tx.to}`}>{tx.to?.slice(0, 20)}...</Link></td>
+                            <td className="py-4 px-6  border-b border-gray-600 text-center">{tx.value} {tx.asset}</td>
+                            <td className="py-4 px-6  border-b border-gray-600">{tx.category}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     )
--- blockexplorer/src/component/Header.jsx
+++ blockexplorer/src/component/Header.jsx
@@ -1,5 +1,6 @@
 import SearchBlock from "./searchBlock"
 import ethereum from "../assets/coins.png"
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return(
@@ -7,7 +8,7 @@ export default function Header() {
             <div className="flex items-center justify-center gap-3">
                 <img src={ethereum} alt="Ethereum Logo" className="w-10 h-10 object-contain" />
                 <div>
-                    <h1 className="text-4xl font-bold text-white">Ethereum Block Explorer</h1>
+                    <Link to={'/'} className="text-4xl font-bold text-white">Ethereum Block Explorer</Link>
                     <h1 className="mt-2 font-bold">Project by <a className="text-blue-300" href="https://github.com/alghifarydaffa62" target="_blank">alghifarydaffa62</a></h1>
                 </div>
                 
